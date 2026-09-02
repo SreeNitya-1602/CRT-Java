dfaf94b194629e704f3ab17d26e73ca78273cd1a
nitya <sreenitya.g1602@gmail.com>
2026-09-02 09:55:41 +0530

    Initial Java programs

---
+++ src/Even_or_Odd.java
@@ -0,0 +1,13 @@
+public class Even_or_Odd {
+    public static void main(String []args)
+    {
+        int x=10;
+        if(x%2==0)
+        {
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String []args)
+    {
+        int x=12;
+        int y=22;
+        int z=x+y;
+        System.out.println("sum="+z);
+    }
+}
